6cd5b870d5982a2168769ae8eba2f30930952924
Naveenasri-T <naveenasrit@gmail.com>
2025-09-21 19:50:24 +0530

    Update models and configuration

--- news/app/db/models.py
+++ news/app/db/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
+
 Base = declarative_base()
 
 class SearchHistory(Base):
